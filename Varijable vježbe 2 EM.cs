3b05dff06e1bb252489bb166d9568b26f0fd2c6f
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2020-11-09 09:08:53 +0100

    Add files via upload

---
+++ Varijable vježbe 2 EM.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varijable_vježbe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 100;
+            int b= -1000;
+            char c = 'a';
+            float d = 3.123f;
+            double e = 3.123456789;
+            bool x = true;
+            string f = "true";
+            
+
+            Console.WriteLine("{0}, {1}, {2},{3}, {4}, {5}, {6}", a, b, c, d, e, x, f );
+            
+        }
+    }
+}
